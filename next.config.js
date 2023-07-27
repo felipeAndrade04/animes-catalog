68b7828b75a8bdce1736b09c92d6b3607cb74b89
Felipe Andrade <flpandrade2011@gmail.com>
2023-07-27 13:35:12 -0300

    build: add less and antd

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
 
-module.exports = nextConfig
+const withLess = require('next-with-less');
+
+module.exports = withLess({
+  // reactStrictMode: true,
+  lessLoaderOptions: {},
+});
