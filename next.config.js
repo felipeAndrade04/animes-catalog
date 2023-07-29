90cd646ec10bd80e443d79e7c0198d2656d0cde2
Felipe Andrade <flpandrade2011@gmail.com>
2023-07-29 12:20:36 -0300

    refactor: improve card component

--- next.config.js
+++ next.config.js
@@ -5,4 +5,8 @@ const withLess = require('next-with-less');
 module.exports = withLess({
   // reactStrictMode: true,
   lessLoaderOptions: {},
+  images: {
+    domains: ['media.kitsu.io', 'kitsu.io', 'avatars.slack-edge.com'],
+    formats: ['image/avif', 'image/webp'],
+  },
 });
